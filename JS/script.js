18e16f7450968891ae051e7c616a474d71b97439
SimoneColombo1 <simcolombo2@gmail.com>
2024-05-23 02:36:29 +0200

    dichiarato l'array e stampato in console

---
+++ JS/script.js
@@ -0,0 +1,48 @@
+
+//? richiamo la sezione nel dom
+const sezione= document.querySelector('#section'); 
+
+//> array del team 
+const team= [ 
+   //< oggetti 
+   { 'img':'img_wayne',
+    'nome': 'Wayne Barnett',
+    'job' : 'Founder & Ceo'
+    },
+    {
+         'img': 'img_angela',
+          'nome': 'Angela Caroll',
+          'job' : 'Chief Editor'
+    },
+    {
+        'img': 'img_walter',
+          'nome': 'Walter Gordon',
+          'job' : 'Office Manager'
+    },
+    {
+        'img': 'img_lopez',
+          'nome': 'Angela Lopez',
+          'job' : 'Social Media Manager'
+    },
+    {
+        'img': 'img_scott',
+          'nome': 'Scott Estrada',
+          'job' : 'Developer'
+    },
+    {
+        'img': 'img_barbara',
+          'nome': 'Barbara Ramos',
+          'job' : 'Graphic Designer'
+    },
+  
+  
+]
+
+//todo Ciclo for per stampare l'array
+
+
+    for(let key in team){
+        console.log(team[key]);
+    }
+  
+    
